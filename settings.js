922ff4d0d5a4803449aa33a7fc9ef04f46204feb
dybot <dybot4444@gmail.com>
2022-06-03 18:57:53 +0700

    dybot1

---
+++ settings.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//---------( APIKEY )---------//
+global.APIs = {
+zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+'https://zenzapis.xyz': 'Your Key',
+}
+
+//---------( SETTING )---------//
+global.owner = ['6282137666455','6281282273655','6281290957514']
+global.premium = ['6282137666456']
+global.ownernomer = '6282137666456'
+global.ownername = 'DrkYrn'
+global.botname = 'DYBOT'
+global.footer = 'DYBOT
+global.thumb = fs.readFileSync('./image/fake.jpg')
+global.veloriy = fs.readFileSync('./image/kagura.mp4')
+global.sessionName = 'session'
+global.packname = 'DY'
+global.author = 'BOT'
+global.sp = '⭔'
+
+//---------( INFO )---------//
+global.youtube = 'https://youtube.com/c/LEX4YOUU'
+global.ig = 'https://instagram.com/Lexxy24'
+global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.myweb = 'https://github.com/Drkyrn'
+global.email = 'darkyorn456@gmail.com'
+global.region = 'indonesia'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.limitawal = {
+premium: "Infinity",
+free: 35,
+}
+
+//---------( RANDOM IMAGE )---------//
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//---------( MESSAGES )---------//
+global.mess = {
+success: '_Done._',
+admin: '_Fitur Khusus Admin Group!_',
+botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
+owner: '_Fitur Khusus Owner Bot_',
+group: '_Fitur Digunakan Hanya Untuk Group!_',
+private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sedang Di Proses..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+}
+
+//---------( PEMBATAS )---------//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
